d8f48fe8d2ec7411db290c15554b8d5eea8587d4
Fabricio C Santos <fsantos.veredinha@gmail.com>
2024-03-14 17:43:59 -0300

    Card
    Modularização do card

--- app/src/main/java/br/com/fiap/calculajurossimples/MainActivity.kt
+++ app/src/main/java/br/com/fiap/calculajurossimples/MainActivity.kt
@@ -3,39 +3,11 @@ package br.com.fiap.calculajurossimples
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableDoubleStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import br.com.fiap.calculajurossimples.calculos.calcularJuros
-import br.com.fiap.calculajurossimples.calculos.calcularMontante
-import br.com.fiap.calculajurossimples.components.CaixaDeEntrada
-import br.com.fiap.calculajurossimples.components.CardResultado
+import br.com.fiap.calculajurossimples.juros.JurosScreen
 import br.com.fiap.calculajurossimples.ui.theme.CalculaJurosSImplesTheme
 
 class MainActivity : ComponentActivity() {
@@ -50,102 +22,6 @@ class MainActivity : ComponentActivity() {
                     JurosScreen()
                 }
             }
-        }
-    }
-}
-
-@Composable
-fun JurosScreen() {
-
-    var capital by remember { mutableStateOf("") }
-    var taxa by remember { mutableStateOf("") }
-    var tempo by remember { mutableStateOf("") }
-    var juros by remember { mutableDoubleStateOf(0.0) }
-    var montante by remember { mutableDoubleStateOf(0.0) }
-
-    Box(
-        modifier = Modifier.padding(16.dp),
-        contentAlignment = Alignment.Center
-    ) {
-        Column() {
-            Text(
-                text = "Cálculo de Juros Simples",
-                modifier = Modifier.fillMaxWidth(),
-                fontSize = 20.sp,
-                color = Color.Red,
-                fontWeight = FontWeight.Bold,
-                textAlign = TextAlign.Center
-            )
-            Spacer(modifier = Modifier.height(32.dp))
-            // Formulário para entrada de dados
-            Card(
-                modifier = Modifier
-                    .fillMaxWidth()
-            ) {
-                Column(modifier = Modifier.padding(16.dp)) {
-                    Text(
-                        text = "Dados do Investimento",
-                        fontWeight = FontWeight.Bold
-                    )
-                    
-                    CaixaDeEntrada(
-                        label = "Valor do investimento",
-                        placeHolder = "Quanto deseja investir?",
-                        value = capital,
-                        keyboardType = KeyboardType.Decimal,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(top = 16.dp),
-                        atualizarValor = {
-                            capital = it
-                        }
-                    )
-                    CaixaDeEntrada(
-                        label = "Taxa de juros mensal",
-                        placeHolder = "Qual a taxa de juros mensal?",
-                        value = taxa,
-                        keyboardType = KeyboardType.Decimal,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(top = 16.dp),
-                        atualizarValor = {
-                            taxa = it
-                        }
-                    )
-                    CaixaDeEntrada(
-                        label = "Período em meses",
-                        placeHolder = "Qual o tempo em meses?",
-                        value = tempo,
-                        keyboardType = KeyboardType.Decimal,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(top = 16.dp),
-                        atualizarValor = {
-                            tempo = it
-                        }
-                    )
-                    Button(
-                        onClick = {
-                            juros = calcularJuros(
-                                capital = capital.toDouble(),
-                                taxa = taxa.toDouble(),
-                                tempo = tempo.toDouble()
-                            )
-                            montante = calcularMontante(
-                                capital = capital.toDouble(),
-                                juros = juros
-                            )
-                        },
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(top = 32.dp)
-                    ) {
-                        Text(text = "CALCULAR")
-                    }
-                }
-            }
-            Spacer(modifier = Modifier.height(16.dp))
-            CardResultado(juros = juros, montante = montante)
         }
     }
 }
